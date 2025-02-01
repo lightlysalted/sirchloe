db3d1002546ece6192892e9fccfde59583276748
lightlysalted <159079841+lightlysalted@users.noreply.github.com>
2025-01-31 18:05:19 -0700

    updated site

---
+++ main.js
@@ -0,0 +1,9 @@
+// noticeBtn.addEventListener("click", function(){
+//     document.getElementById('notice').style.display = 'none';
+// });
+function openNav(){
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav(){
+    document.getElementById("mySidenav").style.width = "0";
+}
